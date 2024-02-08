4722dea7e7eb2483a5139298fa33c5413d675018
Hady Ibrahim <hadyib007@gmail.com>
2024-02-08 13:01:16 -0500

    change Browser router to Hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/dashboard';
 import Login from './components/login';
 import AxiosExample from './components/axiosexample';
