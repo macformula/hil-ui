87a9e83715dfc515a4a0a7a8e05aaefac7a1d19f
Varun Kothandaraman <91991164+Nuravar@users.noreply.github.com>
2024-02-10 19:26:43 -0500

    test commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { ChakraProvider } from '@chakra-ui/react';
 // import theme from '@chakra-ui/theme';
-
+// testing github
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
