4d2cbeaa8cc163e5aed107ef1e56565f1634ed28
Tyler St-Amour <tylerstamour1@gmail.com>
2024-03-31 14:49:58 -0400

    Working websocket example

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -15,37 +15,30 @@ const Dashboard = () => {
         navigate('/');
     };
 
-    const subscription = "";
-
     useEffect(() => {
       const ws = new WebSocket(
-        "ws://api.macformularacing.com/starttest"
+        "wss://api.macformularacing.com/test"
       );
-  
+
       ws.onopen = () => {
-        console.log("Connection Established!");
-        ws.send(JSON.stringify(subscription));
+          console.log("Connection Established!");
+          ws.send("aaaaa")
       };
+
       ws.onmessage = (event) => {
-        const response = JSON.parse(event.data);
-        console.log(response);
-        //ws.close();
+          console.log("received")
+          console.log(event.data);
+          return false
       };
+
       ws.onclose = () => {
         console.log("Connection Closed!");
-        //initWebsocket();
       };
-  
+
       ws.onerror = () => {
         console.log("WS Error");
       };
-  
-      return () => {
-        ws.close();
-      };
     }, []);
-
-
 
     return (
         <VStack h="100vh" p="3">
