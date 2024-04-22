9e24790934d36c1f9e8297d60f89f562e9cad636
Varun Kothandaraman <91991164+Nuravar@users.noreply.github.com>
2024-04-21 23:10:51 -0400

    fix typescript

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,5 +1,5 @@
 // import React from 'react';
-import { Box, Flex, Heading, Image, HStack, VStack, Input, Text, Checkbox, Button, Link, Divider} from '@chakra-ui/react';
+import { Box, Flex, Heading, Image, HStack, VStack, Text, Button, Link, Divider} from '@chakra-ui/react';
 import logo from "../assets/logo.png" 
 import bitmapImage from "../assets/formula-bitmap.png"
 import { FaGithub, FaWrench } from 'react-icons/fa';
